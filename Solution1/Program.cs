62c48d306d2e0bc3ee03cfa58c2dc2d28fca9f5b
Ramedon <info@ramed.one>
2024-11-20 21:55:56 +0500

    ✨ New Solutions

---
+++ Solution1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Solution1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число:");
+        double first_number = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        double second_number = Convert.ToDouble(Console.ReadLine());
+
+        if (second_number == 0 || first_number == 0)
+        {
+            Console.WriteLine("На ноль делить нельзя!");
+        }
+        else
+        {
+            Console.WriteLine("Сумма: " + (first_number + second_number));
+            Console.WriteLine("Разность: " + (first_number - second_number));
+            Console.WriteLine("Произведение: " + (first_number * second_number));
+            Console.WriteLine("Частное: " + (first_number / second_number));
+        }
+    }
+}
---
+++ Solution2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Solution2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            double first_number = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Результат: " + (first_number + 5));
+            Console.WriteLine("Ошибка: Введите корректное число.");
+        }
+    }
+}
